50afa53cb5532196c7019f1dd2134816d054c254
ntoiti <fidesluckia36@gmail.com>
2025-07-04 11:27:45 +0300

    added code

--- routes/countries.js
+++ routes/countries.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
-const countriesController=require('.../controllers/countriesController')
+const countriesController=require('../controllers/countriesController')
 
 // CREATE
 router.post('/',countriesController.createCountry)
