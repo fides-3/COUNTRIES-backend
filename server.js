475b10e1ccba56e29aafd5dbe65b89890390e58d
ntoiti <fidesluckia36@gmail.com>
2025-07-04 11:27:29 +0300

    corrected file path

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors);
 app.use(bodyParser.json);
 app.use('/api/countries',countryRoutes)
 
-const PORT=process.env.PORT||5000;
+const PORT=process.env.PORT || 5000;
 app.listen(PORT , ()=>{
-    console.log('server is running on port  ${PORT}')
+    console.log(`server is running on port ${PORT}`)
 });
